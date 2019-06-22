a61d50e504db5a3b5f8bd5e9c61ad5391d281c87
fatma kübra yalçın <fatma kubra yalcin>
2019-06-22 13:07:16 +0300

    initial commit

---
+++ app/src/main/java/com/fatmakubrayalcin/glutensorgulama1/GlutenNedir.java
@@ -0,0 +1,13 @@
+package com.fatmakubrayalcin.glutensorgulama1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GlutenNedir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gluten_nedir);
+    }
+}
